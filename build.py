066ba513d7912af5bbfb03a5eab73061a81b08e0
Daniel Cook <danielecook@gmail.com>
2016-12-18 19:42:29 -0600

    changes

--- build.py
+++ build.py
@@ -5,12 +5,12 @@ import requests
 import os
 from pprint import pprint as pp
 from dateutil.parser import parse
+import re
 
 token = open(".token", 'r').read()
 
 out = requests.get("https://docs.google.com/spreadsheets/d/1fvURn1gixxC_VbSPr0Ne9dXshffAOKOmpMTmQ9YtGUc/pub?output=tsv")
 
-print out.text
 # Save 
 with open("_data/workflows.tsv", 'w') as f:
     f.write(out.text)
@@ -33,10 +33,14 @@ for repo in DictReader(StringIO(out.text), delimiter = "\t"):
     # Release Info
     release_url = "https://api.github.com/repos/" + username + "/" + title + "/releases/latest"
     release = requests.get(release_url, auth = ("danielecook", token)).json()
-    download_url = release["assets"][0]["browser_download_url"]
-    download_count = release["assets"][0]["download_count"]
-    release_publish_date = release["assets"][0]["created_at"]
-    version = release["tag_name"]
+
+    if 'assets' in release:
+        download_url = release["assets"][0]["browser_download_url"]
+        download_count = release["assets"][0]["download_count"]
+        release_publish_date = release["assets"][0]["created_at"]
+        version = release["tag_name"]
+    else:
+        break
     
 
     # Repo Info
@@ -64,19 +68,24 @@ stars: {stars}
 watchers: {watchers}
 version: {version}
 ---
-
 """.format(**locals())
 
     if not os.path.exists(directory):
         os.makedirs(directory)
-    if not os.path.exists(username):
-        os.makedirs(username)
-    with open(username + "/index.html", 'w+') as f:
+    if not os.path.exists("u/" + username):
+        os.makedirs("u/" + username)
+    with open("u/" + username + "/index.html", 'w+') as f:
         f.write(open("author_index.html").read().replace("<AUTHOR>", username))
     post_filename = "_posts/" + username + "/" + date_submitted + "-" + title + ".md"
-    print post_filename
     with open(post_filename,'w') as f:
         output = front_matter + readme.encode('utf-8').strip()
+        # Identify media and fetch!
+        for i in re.findall("\!\[([^\]])+]\(([^)]+)\)", output):
+            "https://raw.githubusercontent.com/%s/master/README.md" % repo["Github repo"]
+            requests.get("")
+            print i[1]
+            #print dir(i)
+    
         f.write(output)
 
 
